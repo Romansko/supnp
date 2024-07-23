e372d8b49d06b31ea09aeab6adcded75e642b633
Roman Koifman <Romansko@users.noreply.github.com>
2024-07-23 20:23:20 +0300

    CapToken infra

---
+++ upnp/inc/supnp_captoken.h
@@ -0,0 +1,31 @@
+#ifndef SUPNP_CAPTOKEN_H
+#define SUPNP_CAPTOKEN_H
+
+#include "UpnpGlobal.h" /* for UPNP_EXPORT_SPEC */
+#include "upnpconfig.h"
+#ifdef ENABLE_SUPNP
+
+/*!
+ * \addtogroup SUPnP
+ *
+ * \file supnp_captoken.h
+ *
+ * \brief Header file for SUPnP CapToken algorithms.
+ *
+ * \author Roman Koifman
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* ENABLE_SUPNP */
+
+#endif /* SUPNP_CAPTOKEN_H */
---
+++ upnp/src/supnp/supnp_captoken.c
@@ -0,0 +1,29 @@
+/*!
+ * \addtogroup SUPnP
+ *
+ * \file supnp_captoken.c
+ *
+ * \brief source file for SUPnP CapToken algorithms. Implementing logics from
+ * the paper "Kayas, G., Hossain, M., Payton, J., & Islam, S. R. (2021). SUPnP:
+ * Secure Access and Service Registration for UPnP-Enabled Internet of Things.
+ * IEEE Internet of Things Journal, 8(14), 11561-11580."
+ *
+ * \author Roman Koifman
+ */
+#include "supnp_captoken.h"
+
+#ifdef ENABLE_SUPNP
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif /* ENABLE_SUPNP */
